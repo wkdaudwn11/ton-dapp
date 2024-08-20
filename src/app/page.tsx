c542e9687d610374e559c09583e8b7c4e28815c0
MJ <wkdaudwn11@naver.com>
2024-08-20 12:44:14 +0900

    fix: resolve manifest url issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import TonConnector from '@/components/ton-connector';
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TonConnectUIProvider manifestUrl="http://localhost:3000/tonconnect-manifest.json">
+      <TonConnectUIProvider manifestUrl="https://ton-dapp-beige.vercel.app/tonconnect-manifest.json">
         <TonConnector />
       </TonConnectUIProvider>
     </main>
